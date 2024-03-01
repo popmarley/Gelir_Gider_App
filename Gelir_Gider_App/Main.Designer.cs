91367d24be7580f7ee98ce73dc0965949cc9e625
popmarley <hsynozguven@hotmail.com>
2024-03-01 17:14:39 +0300

    Guncelleme (Excelde verileri eklerken işlem türüne göre renklendirme işlevi eklendi)

--- Gelir_Gider_App/Main.Designer.cs
+++ Gelir_Gider_App/Main.Designer.cs
@@ -32,7 +32,31 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.hakkındaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.uygulamaHakkindaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtHarcama = new System.Windows.Forms.TextBox();
+            this.dtTarih = new System.Windows.Forms.DateTimePicker();
+            this.txtTutar = new System.Windows.Forms.TextBox();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Tarih = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.harcamaAlani = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tutar = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.İslem = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.cmbAy = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -41,7 +65,7 @@
             this.hakkındaToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(635, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -60,20 +84,229 @@
             this.uygulamaHakkindaToolStripMenuItem.Text = "Uygulama Hakkında";
             this.uygulamaHakkindaToolStripMenuItem.Click += new System.EventHandler(this.uygulamaHakkindaToolStripMenuItem_Click);
             // 
+            // txtHarcama
+            // 
+            this.txtHarcama.Location = new System.Drawing.Point(60, 126);
+            this.txtHarcama.Name = "txtHarcama";
+            this.txtHarcama.Size = new System.Drawing.Size(98, 20);
+            this.txtHarcama.TabIndex = 1;
+            // 
+            // dtTarih
+            // 
+            this.dtTarih.Location = new System.Drawing.Point(60, 181);
+            this.dtTarih.Name = "dtTarih";
+            this.dtTarih.Size = new System.Drawing.Size(98, 20);
+            this.dtTarih.TabIndex = 2;
+            // 
+            // txtTutar
+            // 
+            this.txtTutar.Location = new System.Drawing.Point(60, 228);
+            this.txtTutar.Name = "txtTutar";
+            this.txtTutar.Size = new System.Drawing.Size(98, 20);
+            this.txtTutar.TabIndex = 1;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(69, 266);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(75, 23);
+            this.btnKaydet.TabIndex = 3;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 26);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Harcama \r\nAdı";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tarih";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Tutar";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Tarih,
+            this.harcamaAlani,
+            this.Tutar,
+            this.İslem});
+            this.dataGridView1.Location = new System.Drawing.Point(192, 53);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(438, 236);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Tarih
+            // 
+            this.Tarih.HeaderText = "Tarih";
+            this.Tarih.Name = "Tarih";
+            // 
+            // harcamaAlani
+            // 
+            this.harcamaAlani.HeaderText = "Harcama Alanı";
+            this.harcamaAlani.Name = "harcamaAlani";
+            // 
+            // Tutar
+            // 
+            this.Tutar.HeaderText = "Tutar";
+            this.Tutar.Name = "Tutar";
+            // 
+            // İslem
+            // 
+            this.İslem.HeaderText = "İşlem";
+            this.İslem.Name = "İslem";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 336);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Geliriniz:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 375);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Gideriniz";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 414);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Kalan";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(80, 336);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "label4";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(80, 414);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "label4";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(80, 375);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "label4";
+            // 
+            // cmbAy
+            // 
+            this.cmbAy.FormattingEnabled = true;
+            this.cmbAy.Location = new System.Drawing.Point(60, 53);
+            this.cmbAy.Name = "cmbAy";
+            this.cmbAy.Size = new System.Drawing.Size(98, 21);
+            this.cmbAy.TabIndex = 7;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(5, 43);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(57, 39);
+            this.label10.TabIndex = 4;
+            this.label10.Text = "İşlem \r\nYapılacak \r\nAy";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(204, 327);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(98, 20);
+            this.textBox3.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(213, 365);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Kaydet";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(155, 330);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(44, 13);
+            this.label11.TabIndex = 4;
+            this.label11.Text = "Gelir Gir";
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(635, 450);
+            this.Controls.Add(this.cmbAy);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.dtTarih);
+            this.Controls.Add(this.txtTutar);
+            this.Controls.Add(this.txtHarcama);
             this.Controls.Add(this.menuStrip1);
             this.DoubleBuffered = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Gelir Gider App | V1.0";
+            this.Text = "Gelir Gider App | V1.1";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -84,6 +317,29 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem hakkındaToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem uygulamaHakkindaToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtHarcama;
+        private System.Windows.Forms.DateTimePicker dtTarih;
+        private System.Windows.Forms.TextBox txtTutar;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tarih;
+        private System.Windows.Forms.DataGridViewTextBoxColumn harcamaAlani;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tutar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn İslem;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox cmbAy;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label11;
     }
 }
 
--- Gelir_Gider_App/Main.cs
+++ Gelir_Gider_App/Main.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using OfficeOpenXml;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LicenseContext = OfficeOpenXml.LicenseContext;
 
 namespace Gelir_Gider_App
 {
@@ -15,6 +18,11 @@ namespace Gelir_Gider_App
         public Main()
         {
             InitializeComponent();
+            // ComboBox'a Ay Listesi Ekleyin
+            for (int i = 1; i <= 12; i++)
+            {
+                cmbAy.Items.Add(System.Globalization.CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(i));
+            }
         }
 
         private void uygulamaHakkindaToolStripMenuItem_Click(object sender, EventArgs e)
@@ -22,5 +30,86 @@ namespace Gelir_Gider_App
             Hakkinda hakkinda = new Hakkinda();
             hakkinda.ShowDialog();
         }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Gelir Gider App.xlsx");
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial; // Geliştirme amacıyla kullanım için lisans matrisini ayarladı
+            using (var package = new ExcelPackage(new FileInfo(filePath)))
+            {
+                // Ay isimlerini sırayla al ve kontrol et
+                var months = Enumerable.Range(1, 12).Select(i => System.Globalization.CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(i).ToUpper()).ToList();
+
+                // Excel dosyasında her bir ay için bir sayfa oluştur
+                foreach (var month in months)
+                {
+                    // Eğer sayfa zaten varsa atla
+                    if (package.Workbook.Worksheets.Any(x => x.Name.ToUpper() == month))
+                        continue;
+
+                    // Sayfa yoksa, yeni bir sayfa ekle
+                    package.Workbook.Worksheets.Add(month);
+                }
+
+                string selectedMonth = cmbAy.SelectedItem.ToString().ToUpper();
+                var worksheet = package.Workbook.Worksheets[selectedMonth] ?? package.Workbook.Worksheets.Add(selectedMonth);
+
+                // Başlıkları Ekle
+                worksheet.Cells["A1"].Value = "Harcama Adı";
+                worksheet.Cells["B1"].Value = "Tarih";
+                worksheet.Cells["C1"].Value = "Tutar";
+                worksheet.Cells["D1"].Value = "İşlem";
+
+                // Sadece A-D sütunları için son dolu satırı bul
+                int lastRowForABCD = 1;
+                for (int i = 1; i <= worksheet.Dimension.End.Row; i++)
+                {
+                    if (worksheet.Cells[i, 1].Value != null || worksheet.Cells[i, 2].Value != null || worksheet.Cells[i, 3].Value != null || worksheet.Cells[i, 4].Value != null)
+                    {
+                        lastRowForABCD = i;
+                    }
+                }
+
+                int row = lastRowForABCD + 1; // Yeni veri eklenecek satır
+
+                // Verileri Ekle
+                worksheet.Cells[$"A{row}"].Value = txtHarcama.Text;
+                worksheet.Cells[$"B{row}"].Value = dtTarih.Value.ToString("dd/MM/yyyy");
+                worksheet.Cells[$"C{row}"].Value = decimal.Parse(txtTutar.Text);
+                // Tutar + veya -'ye göre renk ve metin ayarlama
+                if (decimal.Parse(txtTutar.Text) < 0)
+                {
+                    worksheet.Cells[$"D{row}"].Value = "Para Çıkış";
+                    worksheet.Cells[$"D{row}"].Style.Font.Color.SetColor(System.Drawing.Color.Red);
+                }
+                else
+                {
+                    worksheet.Cells[$"D{row}"].Value = "Para Giriş";
+                    worksheet.Cells[$"D{row}"].Style.Font.Color.SetColor(System.Drawing.Color.Green);
+                }
+
+                // Toplam Formülleri Ekle
+                // Bu kısımlar sabit kalır
+                worksheet.Cells["F7"].Value = "Gelir";
+                worksheet.Cells["F8"].Value = "Gider";
+                worksheet.Cells["F9"].Value = "Kalan";
+                worksheet.Cells["G7"].Formula = "SUMIF(C:C,\">0\")";
+                worksheet.Cells["G8"].Formula = "SUMIF(C:C,\"<0\")";
+                worksheet.Cells["G9"].Formula = "G7+G8"; // Daha doğru formülleme
+                worksheet.Cells["G7"].Style.Font.Color.SetColor(System.Drawing.Color.Green);
+                worksheet.Cells["G8"].Style.Font.Color.SetColor(System.Drawing.Color.Red);
+                worksheet.Cells["G9"].Style.Font.Color.SetColor(System.Drawing.Color.Blue);
+
+                // Değişiklikleri kaydet
+                package.Save();
+            }
+
+            MessageBox.Show("Kayıt Başarılı!");
+            txtHarcama.Clear();
+            txtTutar.Clear();
+        }
+
+
     }
 }
+
